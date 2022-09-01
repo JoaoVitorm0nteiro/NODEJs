96d9bffb6a028d2ec4147e83b25f4a95b9ca8dd8
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-09-01 14:23:21 -0300

    alteração nome de diretorios

---
+++ FundamentosNODE/1_moduloInterno/index.js
@@ -0,0 +1,4 @@
+const meu_modulo = require('./meu_modulo');
+const soma = meu_modulo.soma;
+
+soma(2,5);
---
+++ FundamentosNODE/1_moduloInterno/meu_modulo.js
@@ -0,0 +1,6 @@
+//exportando uma função
+module.exports = {
+    soma(a,b){
+        console.log(`A soma é: ${a+b}`);
+    }
+}
