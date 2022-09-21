e1f88a9b1e4cb86c8dac05f22db6932d945edc70
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-09-21 11:10:12 -0300

    bifurcação rota aritmetica concluida

--- algoritmo_MiniSistema/index.js
+++ algoritmo_MiniSistema/index.js
@@ -2,23 +2,51 @@ const inquirer = require('inquirer')
 const chalk = require('chalk')
 
 const init_menu = () => {
-    inquirer.prompt([{
+    inquirer.prompt([
+        {
         name: 'choice',
         type: 'list',
         message: 'O que deseja fazer ?',
         choices: [
             'Conversor de temperatura',
-            'Calculadora de média aritmetica',
+            'MediaAritmetica',
             'Sair'
         ]
-    }]).then((answer)=>{
+    }
+]).then((answer)=>{
         const choice = answer['choice']
         if(choice === 'Sair'){
             console.log(chalk.bgBlue.black('Obrigado por usar nosso sistema.'));
             process.exit()
+        } else if (choice === 'Conversor de temperatura'){
+           
+        } else if (choice === 'MediaAritmetica'){
+           media();
         }
 
     }).catch(err=>{
+        console.log(err.message);
+    })
+}
+
+//prompt para as notas da media
+const media = () =>{
+     inquirer.prompt([
+        {
+        name:'media1',
+        type:'number',
+        message:'Digite sua primeira nota: '
+    },
+    {
+        name:'media2',
+        type:'number',
+        message:'Digite sua segunda nota: '
+    }
+    ]).then((notas)=>{
+        const media = parseFloat((notas.media1 + notas.media2)/2)
+        console.log(`Sua média é: ${media}`);
+
+    }).catch((err)=>{
         console.log(err.message);
     })
 }
