d8539dd50078a45a1f5bbef90e3783b7f03b36e5
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-09-23 13:07:38 -0300

    segunda parte da estrutura

--- Gerador_CPF/index.js
+++ Gerador_CPF/index.js
@@ -4,18 +4,22 @@ const gerarCpf = () => {
     let x = 0;
     let j = 0;
       for(let i = 0; i<9; i++){
-        newCpf += (Math.floor(Math.random() * (10 - 0)))
+        newCpf += (Math.floor(Math.random() * (10 - 0)));
     }
-    
   //primeiro digito verificador ->
   for(let i=10; i>=2; i--){
     x += newCpf.charAt(j)*i
     j++
   }
-  if(newCpf % 11 === 0 || 1){
-    newCpf += (11 - (newCpf % 11));
-  }
-  console.log(newCpf);    
+  
+     if(x % 11 === 0 || 1){
+    newCpf += 0;
+  } else {
+    newCpf += 11 - (x % 11)
+  }   
+
+
+  console.log(x);    
 } 
 
 //segundo digito verificador
