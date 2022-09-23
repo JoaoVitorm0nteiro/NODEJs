e47dc716e60d1aa18c315a0116a008871917a11b
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-09-23 12:32:20 -0300

    estrutura logica do primeiro digito verificador gerado

--- Gerador_CPF/index.js
+++ Gerador_CPF/index.js
@@ -1,20 +1,24 @@
 //algoritmo para gerar CPF ->
-const gerarCpf = () =>{
+const gerarCpf = () => {
     let newCpf = '';
     let x = 0;
     let j = 0;
       for(let i = 0; i<9; i++){
         newCpf += (Math.floor(Math.random() * (10 - 0)))
     }
-  
-    console.log(newCpf); 
-  
+    
   //primeiro digito verificador ->
   for(let i=10; i>=2; i--){
     x += newCpf.charAt(j)*i
     j++
   }
-  //console.log(x);    
+  if(newCpf % 11 === 0 || 1){
+    newCpf += (11 - (newCpf % 11));
+  }
+  console.log(newCpf);    
 } 
+
+//segundo digito verificador
+
 
 gerarCpf()
