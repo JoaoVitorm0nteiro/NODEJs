404f716eb21b6e2ba63914ce8101d1a6a56102fc
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-11-02 16:08:57 -0300

    add: more functions

--- projeto1_Accounts/index.js
+++ projeto1_Accounts/index.js
@@ -95,6 +95,21 @@ function deposito(){
             return deposito()
         }
 
+        inquirer.prompt([
+            {
+                name:'amount',
+                message:'Quanto você deseja depositar ?'
+            }
+    ]).then((answer)=>{
+
+        const amount = answer['amount']
+
+        //add amount
+        addAmount(accountName, amount);
+        operation();
+
+    }).catch(err => console.log(err))
+
     }).catch()
 }
 
@@ -107,7 +122,37 @@ function checkAccount (accountName){
     return true
 }
 
+function addAmount(accountName, amount){
 
+    const account = getAccount(accountName)
+    if(!amount){
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarder'))
+        return deposito()
+    }
+
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+    
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function(err){
+            console.log(err);
+        }
+    )
+    console.log(chalk.green(`Foi depositado o valor de R$${amount} na sua conta!`));
+
+    console.log(account);
+
+}
+
+function getAccount(accountName){
+    const accountJSON =  fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: 'utf8',
+        flar: 'r'
+    })
+
+    return JSON.parse(accountJSON)
+}
 
 
  operation();
