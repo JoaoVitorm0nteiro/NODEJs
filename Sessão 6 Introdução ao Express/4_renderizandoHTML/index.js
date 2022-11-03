54e0a0d3e637ff7fad9202cdd5bceacada939ba8
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-11-03 09:28:30 -0300

    add comments in section 6: renderizing HTML

--- Sessão 6 Introdução ao Express/4_renderizandoHTML/index.js
+++ Sessão 6 Introdução ao Express/4_renderizandoHTML/index.js
@@ -1,13 +1,19 @@
+//modulos
 const express = require('express');
 const path = require('path');
-const port = 3000;
-const app = express();
-const basepath = path.join(__dirname, 'template')
 
+//variaveis base
+const app = express();
+const port = 3000;
+const basepath = path.join(__dirname, 'template');
+
+//Aplicação de fato
 app.get('/', (req, res)=>{
     res.sendFile(`${basepath}/index.html`)
 })
 
+//exibido
 app.listen(port, ()=>{
-    console.log(`Rodando na porta:${port}`);
-})
+    /*exebido no console ->*/ console.log(`Rodando na porta:${port}`);
+});
+
