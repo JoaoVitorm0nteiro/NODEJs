1dc0d868a94fb38086efa9a3c5a2531f897dd212
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-11-07 14:24:42 -0300

    add: alterações sintaxe

--- buscanomes/teste.js
+++ buscanomes/teste.js
@@ -23,8 +23,8 @@ davi
 `
     const arrayNomes = StrNomes.split('\n').filter(Boolean);
     const arrayListaFuncs = StrlistaFuncs.split('\n').filter(Boolean);
-
     let nomes = '';
+    
     for(let j=0; j<arrayNomes.length; j++){
         for(let i = 0; i<arrayListaFuncs.length; i++){
             if(arrayNomes[j] === arrayListaFuncs[i]){
