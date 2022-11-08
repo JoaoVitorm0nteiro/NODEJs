98a0f878e9162d03020d2ace2ee0d531b5a375b1
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-11-07 22:22:07 -0300

    add: mudanças de sintaxe

--- buscanomes/teste.js
+++ buscanomes/teste.js
@@ -1,25 +1,7 @@
     const fs = require('fs');
     const StrNomes = `
-joao vitor
-teste1
-teste2
-teste3
-teste4
-grazi
-teste5
-teste8
-lucas
-davi
 `
     const StrlistaFuncs = `
-teste1
-grazi
-teste2
-teste3
-teste4
-joao vitor
-lucas
-davi
 `
     const arrayNomes = StrNomes.split('\n').filter(Boolean);
     const arrayListaFuncs = StrlistaFuncs.split('\n').filter(Boolean);
@@ -34,7 +16,10 @@ davi
         }
     }
 fs.writeFile('nomes.txt', nomes, (err)=>{
-    if (err) {
+    if (err) 
         console.log(err);
+    else{
+        console.log('Arquivo gerado com sucesso !');
     }
+    
 });
