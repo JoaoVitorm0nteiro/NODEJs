54bd8b727f38b18f5694c294002fb01d61b68f47
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-12-20 14:31:02 -0300

    fazendo testes ao renderizar o HTML com node

---
+++ Sessão 6 Introdução ao Express/4_renderizandoHTML/teste/index.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const path = require ('path');
+
+const port = 3000;
+const app = express();
+
+const basepath = path.join(__dirname, 'template')
+
+app.get('/', (req, res)=>{
+    res.sendFile(`${basepath}/index.html`)
+})
+
+app.listen(port, ()=>{
+    console.log(`App rodando na porta ${port}`);
+})
