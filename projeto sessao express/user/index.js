09e95d7f43f42f4e84f9f9ed8c63b19418e09b11
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-01-05 16:21:05 -0300

    PRATICA DE CONCLUSAO DA SESSAO INTRODUÇÃO AO EXPRESS

--- projeto sessao express/user/index.js
+++ projeto sessao express/user/index.js
@@ -1,12 +1,15 @@
+//arquivo com o modulo de rotas
 const express = require ('express');
+const { appendFile } = require('fs');
 const router = express.Router();
 const path = require ('path');
 
-const basepath = path.join(__dirname, '');
+const basepath = path.join(__dirname, '../template');
 
-
-router.get('/user', (req, res)=>{
+router.get('/:id', (req, res)=>{
+    const id = req.params.id
     res.sendFile(`${basepath}/user.html`);
+    console.log(`O numero do usuario requisitado é: ${id}`);
 })
 
 module.exports = router;
