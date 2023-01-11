f6237b9b8674750aec6f68870b1997edeafde656
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-01-11 16:05:20 -0300

    usando condicionais

---
+++ Sessão 7 TemplateEngine/4_condicionaisHandlebars/index.js
@@ -0,0 +1,38 @@
+const express = require ('express');
+const exphbs = require ('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.get('/dashboard', (req,res)=>{
+    res.render('dashboard')
+})
+
+app.get('/', (req,res)=>{
+    
+const user = {
+    name: 'João Vitor',
+    surname: "",
+    age: "21",  
+    course: "ADS"
+
+}
+
+const palavra = 'teste';
+
+
+const auth = true;
+
+
+
+    res.render('home', {user: user, palavra, auth})
+})
+
+app.listen(port, ()=>{
+    console.log(`Serviço executando na porta: ${port}`);
+})
