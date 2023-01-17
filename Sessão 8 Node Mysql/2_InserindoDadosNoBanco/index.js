5476e1703e0ea7b4e94ca37bcf37835187d83c4c
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-01-17 15:59:00 -0300

    alteração nome da pasta

---
+++ Sessão 8 Node Mysql/2_InserindoDadosNoBanco/index.js
@@ -0,0 +1,34 @@
+const express = require ('express')
+const exphbs = require ('express-handlebars')
+const mysql = require ('mysql')
+const port = 3000;
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req,res)=>{
+    res.render('home')  
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql2',
+})
+
+conn.connect((err) => {
+    if(err){
+        console.log(err);
+    }
+
+    console.log('Conexão estabelecida com sucesso.');
+
+    app.listen(port, ()=>{
+        console.log(`serviço executando na porta: ${port}`);
+    })
+})
