8938abcc15c040c5e3fd91c1e0f5d941e75c3fbd
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-01-17 22:48:59 -0300

    Teste com script de conexão ao BD

---
+++ scriptbd.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql')
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'teste',
+    database: 'nodemysql2',
+})
+
+conn.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Conectado!');
+    }
+})
+
+const query = ' CREATE TABLE Livros (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, Titulo VARCHAR(255), Autor VARCHAR(255));'
+conn.query(query, function (err) {
+    if (err) {
+        console.log(err);
+    }else{
+        console.log('Dados inseridos');
+    }
+})
