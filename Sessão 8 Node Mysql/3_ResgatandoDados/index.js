90407193db37b7cf27e0aa858f1f616092a72005
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-01-18 11:18:35 -0300

    puxando dados do banco

---
+++ Sessão 8 Node Mysql/3_ResgatandoDados/index.js
@@ -0,0 +1,72 @@
+const express = require ('express')
+const exphbs = require ('express-handlebars')
+const mysql = require ('mysql')
+const port = 3000;
+
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended:true
+    })
+)
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req,res)=>{
+    res.render('home')  
+})
+
+app.post('/books/insertbook', (req, res)=>{
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const query = `INSERT INTO Livros (title, pageqty) VALUES ('${title}', '${pageqty}');`
+
+    conn.query(query, function (err){
+        if (err){
+            console.log(err);
+        }
+        res.redirect('/estante');
+
+    })
+
+})
+
+app.get('/estante',(req,res)=>{
+    const QUERY = 'SELECT * FROM Livros;'
+
+    conn.query(QUERY, function(err, data){
+        if(err){
+            console.log(err);
+            return;
+        }
+            const books = data
+            console.log(books);
+    })
+    res.render('estante')
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql2',
+})
+
+conn.connect((err) => {
+    if(err){
+        console.log(err);
+    }
+
+    console.log('Conexão estabelecida com sucesso.');
+
+    app.listen(port, ()=>{
+        console.log(`serviço executando na porta: ${port}`);
+    })
+})
